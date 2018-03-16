6a364d7b9d5846b349acb91dfd8932849bbfc8ae
Pavel Djundik <github@xpaw.me>
2018-03-16 16:10:28 +0200

    Remove nick editor

--- client/js/utils.js
+++ client/js/utils.js
@@ -19,8 +19,6 @@ module.exports = {
 	hasRoleInChannel,
 	move,
 	resetHeight,
-	setNick,
-	toggleNickEditor,
 	toggleNotificationMarkers,
 	requestIdleCallback,
 };
@@ -81,19 +79,6 @@ function join(args) {
 			chan.trigger("click");
 		}
 	}
-}
-
-function toggleNickEditor(toggle) {
-	$("#nick").toggleClass("editable", toggle);
-	$("#nick-value").prop("contenteditable", toggle);
-}
-
-function setNick(nick) {
-	// Closes the nick editor when canceling, changing channel, or when a nick
-	// is set in a different tab / browser / device.
-	toggleNickEditor(false);
-
-	$("#nick-value").text(nick);
 }
 
 const favicon = $("#favicon");
